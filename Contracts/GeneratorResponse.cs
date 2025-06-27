285006b6f02a29108115b8b7ed7aca09e349b6f0
prokhorfedor <prokhorfedor.ua@gmail.com>
2025-06-27 17:48:35 +0300

    minor fixes

--- Contracts/GeneratorResponse.cs
+++ Contracts/GeneratorResponse.cs
@@ -2,8 +2,20 @@
 
 public class GeneratorResponse
 {
+    /// <summary>
+    /// Work orders processed
+    /// </summary>
     public int WorkOrdersCount { get; set; }
+    /// <summary>
+    /// Total Serial Numbers generated
+    /// </summary>
     public decimal SerialNumbersGeneratedCount { get; set; }
+    /// <summary>
+    /// Last generated Serial Number
+    /// </summary>
     public string LastGeneratedSerialNumber { get; set; } = string.Empty;
+    /// <summary>
+    /// File path with newly generated csv-file
+    /// </summary>
     public string FilePath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 }
--- SerialNumberGeneratorAPI/Program.cs
+++ SerialNumberGeneratorAPI/Program.cs
@@ -24,19 +24,11 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
+        app.UseSwagger();
+        app.UseSwaggerUI();
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
-
         app.MapControllers();
-
         app.Run();
     }
 }
