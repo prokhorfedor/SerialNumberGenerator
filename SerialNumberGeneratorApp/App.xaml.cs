2acaf6e7a48a1537a2d48c70541d8d0c8d077068
Fedir <prokhorfedor.ua@gmail.com>
2025-07-02 13:02:41 -0700

    add desktop app

---
+++ SerialNumberGeneratorApp/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Service;
+
+namespace SerialNumberGeneratorApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private IServiceProvider _serviceProvider;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        // Add configuration
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddJsonFile("appsettings.json", optional: false);
+        IConfiguration configuration = configurationBuilder.Build();
+        services.AddSingleton<IConfiguration>(configuration);
+        
+        // Configure Logging
+        services.AddLogging();
+
+        // Register Services
+        services.AddSingleton<ISerialNumberGenerator, SerialNumberGenerator>();
+
+        // Register ViewModels
+        services.AddDbContextPool<WorkOrderContext>(options => options.UseSqlServer(
+            configuration.GetConnectionString("CubeConnectionString")
+        ));
+        // Register Views
+        services.AddSingleton<MainWindow>();
+    }
+
+    private void OnExit(object sender, ExitEventArgs e)
+    {
+        // Dispose of services if needed
+        if (_serviceProvider is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }  
+}
---
+++ SerialNumberGeneratorApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Service;
+
+namespace SerialNumberGeneratorApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly ISerialNumberGenerator _serialNumberGenerator;
+    public MainWindow(ISerialNumberGenerator serialNumberGenerator)
+    {
+        InitializeComponent();
+        this.ResizeMode = ResizeMode.NoResize;
+        this.Height = 500;
+        this.Width = 400;
+        _serialNumberGenerator = serialNumberGenerator;
+        this.GeneratorInfoBox.Text = string.Empty;
+    }
+
+    private async void GenerateButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var sb = new StringBuilder();
+        try
+        {
+            sb.AppendLine("Starting generation of serial numbers...");
+            this.GeneratorInfoBox.Text = sb.ToString();
+            var response = await _serialNumberGenerator.GenerateSerialNumbersFileAsync(string.Empty);
+            sb.AppendLine($"Serial numbers generated successfully. Saved file to {response.FilePath}");
+            sb.AppendLine($"Total work orders processed: {response.WorkOrdersCount}");
+            sb.AppendLine($"Total serial numbers generated: {response.SerialNumbersGeneratedCount}");
+            sb.AppendLine($"Last generated serial number: {response.LastGeneratedSerialNumber}");
+            this.GeneratorInfoBox.Text = sb.ToString();
+        }
+        catch(Exception exception)
+        {
+            sb.AppendLine("Failed to generate serial numbers.");
+            sb.AppendLine(exception.Message);
+            this.GeneratorInfoBox.Text = sb.ToString();
+            Console.WriteLine(exception);
+        }
+    }
+}
