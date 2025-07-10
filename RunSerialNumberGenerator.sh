c9b4b6704b0b5619526ee3dade9672b5954a3a39
prokhorfedor <prokhorfedor.ua@gmail.com>
2025-07-10 21:51:45 +0300

    Delete RunSerialNumberGenerator.sh

--- RunSerialNumberGenerator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dotnet run --project SerialNumberGeneratorAPI/SerialNumberGeneratorAPI.csproj --launch-profile 'SerialNumberGenerator' &
-PID=$!
-wait $PID
-open 'http://localhost:53348/swagger/index.html'
